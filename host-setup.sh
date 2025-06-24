815fe4f12921c84e127c27485ba07b55a29e68a7
Leshna <Leshna@node0.besteffortlan.iommuvm-nic-pg0.utah.cloudlab.us>
2025-06-24 03:15:58 -0600

    nested translation xml

--- host-setup.sh
+++ host-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo mst start
-sudo bash -c "echo 4 > /sys/class/net/enp101s0f1np1/device/sriov_numvfs"
+sudo bash -c "echo 4 > /sys/class/net/eno12409np1/device/sriov_numvfs"
 echo "lspci"
 sudo lspci -D | grep Mellanox
 
-sudo ifconfig enp101s0f1np1 mtu 4000
+sudo ifconfig eno12409np1 mtu 4000
 
-echo "detach device"
-sudo virsh nodedev-detach pci_0000_65_00_6
+#echo "detach device"
+#sudo virsh nodedev-detach pci_0000_65_00_6
